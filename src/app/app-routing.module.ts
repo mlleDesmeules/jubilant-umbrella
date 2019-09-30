9628ef3aef3de14eadfda8d1a946e6550788d190
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-09-29 21:53:18 -0400

    use layout and invoice module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: ``,
+        component: DefaultComponent,
+        loadChildren: () => import(`./invoices/invoices.module`).then(m => m.InvoicesModule),
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InvoicesModule } from './invoices/invoices.module';
+
+import { DefaultComponent } from './layout/default/default.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+
+        DefaultComponent,
+        HeaderComponent,
+        FooterComponent,
+    ],
+    imports     : [
+        BrowserModule,
+        AppRoutingModule,
+
+        InvoicesModule,
+    ],
+    providers   : [],
+    bootstrap   : [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
